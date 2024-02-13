32e12883f375ec5d528c5711cd47ba14c2d8195c
Yaqoob <social.yaqoob@gmail.com>
2024-02-13 17:50:57 +0500

    fcm

--- app/src/main/java/com/hypersoft/fcm/MainActivity.kt
+++ app/src/main/java/com/hypersoft/fcm/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.hypersoft.fcm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        SonicFCM.setupFCM(this,packageName)
+
+        findViewById<TextView>(R.id.btnNext).setOnClickListener {
+            startActivity(Intent())
+        }
     }
 }
